08a6cf76c5c03d704a044c7bcadf9fcc6a4eab0f
cookie1170 <maksimhovanets@gmail.com>
2024-06-16 12:39:32 +1000

    it works (barely)

---
+++ scenes/reusable/enemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+#literally just copy pasted this from a yt tutorial
+
+@export var target: Node2D
+
+var speed = 300
+var acceleration = 7
+
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	direction = navigation_agent.get_next_path_position() - global_position
+	direction = direction.normalized()
+	
+	velocity = velocity.lerp(direction * speed, acceleration * delta)
+	
+	move_and_slide()
+
+func _on_timer_timeout():
+	navigation_agent.target_position = target.global_position
+	
+func get_hit():
+	queue_free()
--- scripts/controller.gd
+++ scripts/controller.gd
@@ -1,51 +1,84 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -900.0
+var speed = 450.0
+var jump_velocity = -900.0
+var accel = 25
+var decel = 40
+
 
 @onready var sprite = $sprite
+@onready var atk_area = $atk_area
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumped = false
 var released_jump = false
 var j_buffered = false
+var attacking = false
 
 
+func _ready():
+	remove_child(atk_area)
+
+func attack():
+	attacking = true
+	add_child(atk_area)
+	sprite.play("attack")
+	await sprite.animation_finished
+	attacking = false
+	remove_child(atk_area)
+	
+	
 func _physics_process(delta):
-	# Add the gravity.
+	# gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") or j_buffered:
+	
+	
+	#jump spaghetti don't look at this please i'm begging you
+	if Input.is_action_just_pressed("jump") or j_buffered and Input.is_action_pressed("jump"):
 		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			velocity.y = jump_velocity
 			jumped = true
-			sprite.play("jump")
+			if not attacking:
+				sprite.play("jump")
 		if not is_on_floor() and not j_buffered:
 			j_buffered = true
-			await get_tree().create_timer(.15).timeout
+			await get_tree().create_timer(.2).timeout
 			j_buffered = false
-	if not is_on_floor() and not jumped:
-		sprite.play("fall")
-	if not Input.is_action_pressed("ui_accept") and jumped:
+	if not Input.is_action_pressed("jump") and jumped:
 		velocity.y -= velocity.y/2
 		jumped = false
 		gravity *= 2.5
 	if is_on_floor():
 		gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	if velocity.y >= 0:
+		jumped = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_just_pressed("attack") and not attacking:
+		attack()
+
+	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
-		if is_on_floor() and not jumped :
+		velocity.x = move_toward(velocity.x, direction * speed, accel)
+		if direction == -1:
+			sprite.flip_h = true
+			atk_area.position.x = -60
+		if direction == 1:
+			sprite.flip_h = false
+			atk_area.position.x = 60
+		if is_on_floor() and not attacking:
 			sprite.play("run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor() and not jumped:
+	if !direction:
+		velocity.x = move_toward(velocity.x, 0, decel)
+		if not attacking and not jumped:
 			sprite.play("idle")
-
+	
+	if not is_on_floor() and not jumped and not attacking:
+		sprite.play("fall")
+	
 	move_and_slide()
+
+
+func _on_atk_entered(body):
+	body.get_hit()
